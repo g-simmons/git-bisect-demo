00d60442ccaf0e809ec436bd5109802b21764997
Gabriel Simmons <simmons.gabe@gmail.com>
2024-05-01 14:41:57 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math-package',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    description='A toy project to do math',
+    install_requires=[
+    ],
+    python_requires='>=3.11',
+)
