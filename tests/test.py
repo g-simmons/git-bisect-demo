0cdc0fe1c5431192be101d6c4ecb3ca5aa644f0b
Gabriel Simmons <simmons.gabe@gmail.com>
2024-05-01 14:53:17 -0700

    update tests

--- tests/test.py
+++ tests/test.py
@@ -3,21 +3,21 @@ from math_package.number import Number
 
 def test_one_plus_one():
     try:
-        assert Number(1).plus_one() == 2
+        assert Number(1).plus_one() == Number(2)
     except AssertionError:
         exit(1)
 
 
 def test_two_plus_one():
     try:
-        assert Number(2).plus_one() == 3
+        assert Number(2).plus_one() == Number(3)
     except AssertionError:
         exit(1)
 
 
 def test_one_plus_three():
     try:
-        assert Number(1).plus_three() == 3
+        assert Number(1).plus_three() == Number(4)
     except AssertionError:
         exit(1)
 
