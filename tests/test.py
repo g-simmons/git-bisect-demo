f0d15f7daf24cb84537200beb005e08231468686
Gabriel Simmons <simmons.gabe@gmail.com>
2024-05-01 14:50:15 -0700

    update number

--- tests/test.py
+++ tests/test.py
@@ -8,8 +8,15 @@ def test_one_plus_one():
         exit(1)
 
 
-def test_one_plus_two():
+def test_two_plus_one():
     try:
-        assert Number(1).plus_one() == 3
+        assert Number(2).plus_one() == 3
+    except AssertionError:
+        exit(1)
+
+
+def test_one_plus_three():
+    try:
+        assert Number(1).plus_three() == 3
     except AssertionError:
         exit(1)
